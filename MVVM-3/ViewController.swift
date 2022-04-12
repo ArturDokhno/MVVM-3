9af2f36f0027686b25aad6e470e53b24f03459e4
ArturDokhno <artur.dohno@icloud.com>
2022-04-12 13:52:20 +0500

    Initial Commit

---
+++ MVVM-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-3
+//
+//  Created by Артур Дохно on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
